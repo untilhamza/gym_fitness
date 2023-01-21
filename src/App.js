aa4696a739c6a78eeb6d7f2fa44887f3b85678a7
Hamza <untilhamza@gmail.com>
2023-01-21 15:42:18 +0900

    provide missing exercise video props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import {Box} from  '@mui/material';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
-import Exercises from './components/Exercises';
-import ExerciseVideos from './components/ExerciseVideos';
-import NotFound from './components/NotFound';
-import './App.css';
-import Authentication from './components/Authentication';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import Exercises from "./components/Exercises";
+import ExerciseVideos from "./components/ExerciseVideos";
+import NotFound from "./components/NotFound";
+import "./App.css";
+import Authentication from "./components/Authentication";
 
 const App = () => {
-    return ( 
-        <>
-<Box width = "400px" sx={{width: { xl: '1488px'}}} m="auto">
-   
-    <Navbar/>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        <Route path="/Login" element={<LoginForm/>} />
-        <Route path="/signup" element={<SignupForm/>} />
-        <Route path="/exercises" element={<Exercises/>} />
-        <Route path="/videos" element={<ExerciseVideos/>} />
-        <Route path="/auth" element={<Authentication/>} />
-        <Route path="*" element={<NotFound/>} />
-    </Routes>
-    
-
-    <Footer/>
-</Box>
-</>
+  //is the user logged in?
+  //if not take them to login page or signup page
+  // if logged in , take them to the home page
+  return (
+    <>
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+          <Route path="/Login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/exercises" element={<Exercises />} />
+          <Route
+            path="/videos"
+            element={<ExerciseVideos exerciseVideos={[]} name={"user"} />}
+          />
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </>
   );
-}
- 
+};
+
 export default App;
