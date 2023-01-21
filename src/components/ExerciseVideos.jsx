48d1b99d0737c62a5667500172ccb94beb3795eb
Hamza <untilhamza@gmail.com>
2023-01-21 15:43:19 +0900

    fetch youtube vids

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,8 +1,61 @@
-import React from 'react';
-import { Typography, Box, Stack } from '@mui/material';
-import Loader from './Loader';
+import React, { useState, useEffect } from "react";
+import { Typography, Box, Stack } from "@mui/material";
+import Loader from "./Loader";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
+//exerciseVideos is an array of objects with the following structure:
+// {
+//   "video": {
+//     "videoId": "string",
+//     "title": "string",
+//     "channelName": "string",
+//     "thumbnails": [
+//       {
+//         "url": "string"
+//       }
+//     ]
 
-const ExerciseVideos = ({ exerciseVideos, name }) => {
+const videoSample = {
+  video: {
+    channelId: "UCy0-3xdx7dTXMYmJc1jMbBQ",
+    channelName: "Buddy Fitness",
+    description: "CONTENT OF ",
+    lengthText: "8:17",
+    publishedTimeText: "2 years ago",
+    thumbnails: [
+      {
+        height: 202,
+        url: "https://i.ytimg.com/vi/PY6DrbX4W4o/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLBmhoUBCBiZPTF3QxlKNXMxXhR96A",
+        width: 360,
+      },
+      {
+        height: 404,
+        url: "https://i.ytimg.com/vi/PY6DrbX4W4o/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAK0mDDSVe9O_Q5FDjeA8zVLovxMg",
+        width: 720,
+      },
+    ],
+    title:
+      "Full Week Gym Workout Plan | Week Schedule For Gym Workout | Buddy Fitness",
+    videoId: "PY6DrbX4W4o",
+    viewCountText: "11,708,517 views",
+  },
+};
+
+const ExerciseVideos = ({ exerciseVideos: videos, name }) => {
+  const [exerciseVideos, setexerciseVideos] = useState([]);
+  const [exerciseDetail, setExerciseDetail] = useState({}); // provide some intial data for this
+
+  useEffect(() => {
+    const youtubeSearchUrl =
+      "https://youtube-search-and-download.p.rapidapi.com";
+    fetchData(
+      `${youtubeSearchUrl}/channel?id=UCqjwF8rxRsotnojGl4gM0Zw&maxResults=10&order=date`,
+      youtubeOptions
+    ).then((result) => {
+      console.log("result", result);
+      setexerciseVideos(result.contents);
+    });
+  }, []);
+
   if (!exerciseVideos.length) return <Loader />;
 
   return (
@@ -19,14 +72,14 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         </span>{" "}
         exercise videos
       </Typography>
-    
+
       <Stack
         sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
         justifyContent="flex-start"
         flexWrap="wrap"
         alignItems="center"
       >
-        {exerciseVideos?.slice(0, 3)?.map((item, index) => (
+        {exerciseVideos?.slice(0, 9)?.map((item, index) => (
           <a
             key={index}
             className="exercise-video"
